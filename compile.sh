faffe51b31b72c87cdf98440cae47a75f190ae56
Andrew <andrew2085@gmail.com>
2020-05-19 19:55:36 -0400

    x

--- compile.sh
+++ compile.sh
@@ -3,6 +3,6 @@
 halcompile mpg_p2s.comp
 
 gcc -g -I/usr/include -I/usr/include/linuxcnc -URTAPI -U__MODULE__ -DULAPI -Wl,-rpath,/lib -L/lib -c mpg_p2s.c
-g++ -g -std=c++17 -c mpg_logic.cpp
+g++ -I/usr/include/linuxcnc -g -std=c++17 -c mpg_logic.cpp
 
 g++ mpg_p2s.o mpg_logic.o -llinuxcnchal -lusb-1.0 -lnml -llinuxcnc -ltirpc -o mpg_p2s
